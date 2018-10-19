4be11db89c223a2cf027d82f1711bbe6ed93d9ec
Andreas Fleig <afleig@pivotal.io>
2018-10-19 16:11:20 +0200

    Add empty provider project

---
+++ pact-provider/src/main/java/com/example/pactprovider/PactproviderApplication.java
@@ -0,0 +1,12 @@
+package com.example.pactprovider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PactproviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PactproviderApplication.class, args);
+    }
+}
